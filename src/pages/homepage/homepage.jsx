6058b1bee37efd7142eab5f63840fb514e661d5e
Sergio Barria <sergio.barria@icloud.com>
2021-02-06 18:04:28 -0500

    added component structure and styles for homepage

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Components
+import Directory from '../../components/directory/directory';
+
+// Styles
+import './homepage.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
