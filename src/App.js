913e76c39a2707a3dd245487e1287f70a4fc16a6
Sergio Barria <sergio.barria@icloud.com>
2021-02-07 17:39:08 -0500

    added basic form for sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
 import Header from './components/header/header';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 import { Route, Switch } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUp} />
       </Switch>
     </>
   );
